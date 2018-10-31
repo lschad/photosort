1b214fae7a4ea9d5027d17345d2fcedbbb9a1e80
Lucas Schad <schad.lucas@gmail.com>
2018-10-31 16:51:43 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,129 +1,98 @@
-'use strict'
+(function () {
+  'use strict'
 
-let fs = require('fs');
-let util = require('./utils');
-var mkdirp = require('mkdirp');
+  let fs = require('fs');
+  let util = require('./utils');
+  var mkdirp = require('mkdirp');
 
-let source = 'C:\\Users\\lsc\\Downloads\\takeout\\Google Photos';
-let targetBase = 'E:\\SortedPhotos';
+  let source = 'C:\\Users\\lsc\\Downloads\\takeout\\Google Photos';
+  let targetBase = 'E:\\SortedPhotos';
 
+  let getTarget = function (obj) {
+    let exif = obj.exif;
+    let file = obj.file;
 
-let getTarget = function (obj) {
-  let exif = obj.exif;
-  let file = obj.file;
+    let dt = util.stringToDateTime(exif.DateTime);
+    let target = targetBase + '\\unknown';
+    if (!!dt) {
+      let id = exif['ImageUniqueId'] || util.uuidv4();
+      let fileName = id + '.ext';
+      let filePath = targetBase + '\\' + dt.getFullYear() + '\\' + (dt.getMonth() + 1);
 
-  let dt = util.stringToDateTime(exif.DateTime);
-  let target = targetBase + '\\unknown';
-  if (!!dt) {
-    let id = exif['ImageUniqueId'] || util.uuidv4();
-    let fileName = id + '.ext';
-    let filePath = targetBase + '\\' + dt.getFullYear() + '\\' + (dt.getMonth() + 1);
-
-    target = filePath + '\\' + fileName;
-  }
-
-  obj.target = target;
-  return util.simplePromise(obj);
-};
-
-let updateImageObject = function (obj) {
-  return new Promise(function (resolve, reject) {
-
-    if (!obj.ImageUniqueId) {
-      obj.ImageUniqueId = util.uuidv4('');
-    }
-    obj.DateTime = util.stringToDateTime(obj.DateTime);
-    if (!!obj.DateTime) {
-      let t = util.targetPathFromImageObj(targetBase, obj);
-      obj.Filename = t.fileName;
-      obj.TargetBase = t.basePath;
-      obj.TargetPath = t.fullPath();
-    } else {
-      obj.Filename = `${obj.ImageUniqueId}.${obj.Extension.toLowerCase()}`
-      obj.TargetBase = `${targetBase}\\unknown`;
-      obj.TargetPath = `${obj.TargetBase}\\${obj.Filename}`;
+      target = filePath + '\\' + fileName;
     }
 
-    resolve(obj);
-  });
-};
+    obj.target = target;
+    return util.simplePromise(obj);
+  };
 
-let makeFolderIfNeccesary = function (obj) {
-  return new Promise(function (resolve, reject) {
-    mkdirp(obj.TargetBase, function (err) {
-      if (!err) resolve(obj); else reject(err);
-    });
-  });
-
-};
-
-let copyImage = function (obj) {
-  return new Promise(function (resolve, reject) {
-    var read = fs.createReadStream(obj.SourcePath);
-    var write = fs.createWriteStream(obj.TargetPath)
-
-    read.on('error', reject);
-    write.on('error', reject);
-    write.on('close', function () { resolve(obj); });
-
-    read.pipe(write);
-  });
-};
-
-
-let printt = function (txt) {
-  return function (obj) {
+  let updateImageObject = function (obj) {
     return new Promise(function (resolve, reject) {
-      console.info(txt);
+
+      if (!obj.ImageUniqueId) {
+        obj.ImageUniqueId = util.uuidv4('');
+      }
+      obj.DateTime = util.stringToDateTime(obj.DateTime);
+      if (!!obj.DateTime) {
+        let t = util.targetPathFromImageObj(targetBase, obj);
+        obj.Filename = t.fileName;
+        obj.TargetBase = t.basePath;
+        obj.TargetPath = t.fullPath();
+      } else {
+        obj.Filename = `${obj.ImageUniqueId}.${obj.Extension.toLowerCase()}`
+        obj.TargetBase = `${targetBase}\\unknown`;
+        obj.TargetPath = `${obj.TargetBase}\\${obj.Filename}`;
+      }
+
       resolve(obj);
     });
   };
-};
-let printErr = function (err) {
-  return new Promise(function (resolve, reject) {
-    console.error(`error: ${err}`);
-    reject(err);
-  });
-};
 
-
-let gindex = 0;
-let processImage = function (file) {
-  return new Promise(function (resolve, reject) {
-
-    util.getImageObject(`${source}\\${file}`)
-      .then(updateImageObject)
-      .then(makeFolderIfNeccesary)
-      .then(copyImage)
-      .then(function (obj) {
-        console.info(`[${gindex++}] Copied image  ${obj.Filename}  to  ${obj.TargetBase}`);
-        return util.simplePromise(obj);
-      })
-      .then(resolve)
-      .catch(function (err) {
-        console.error("RIP:" + err)
-        resolve();
+  let makeFolderIfNeccesary = function (obj) {
+    return new Promise(function (resolve, reject) {
+      mkdirp(obj.TargetBase, function (err) {
+        if (!err) resolve(obj); else reject(err);
       });
-  });
-};
+    });
 
+  };
 
-function processArray(array, fn) {
-  var index = 0;
+  let copyImage = function (obj) {
+    return new Promise(function (resolve, reject) {
+      var read = fs.createReadStream(obj.SourcePath);
+      var write = fs.createWriteStream(obj.TargetPath)
 
-  return new Promise(function (resolve, reject) {
+      read.on('error', reject);
+      write.on('error', reject);
+      write.on('close', function () { resolve(obj); });
 
-    function next() {
-      if (index < array.length) {
-        fn(array[index++]).then(next, reject);
-      } else {
-        resolve();
-      }
-    }
-    next();
-  });
-}
+      read.pipe(write);
+    });
+  };
 
-let files = fs.readdirSync(source);
+  let run = function () {
+    let files = fs.readdirSync(source);
+    let gindex = 0;
 
-processArray(files, processImage);
+    util.processArray(files, function (file) {
+      return new Promise(function (resolve, reject) {
+
+        util.getImageObject(`${source}\\${file}`)
+          .then(updateImageObject)
+          .then(makeFolderIfNeccesary)
+          .then(copyImage)
+          .then(function (obj) {
+            console.info(`[${gindex++}] Copied image  ${obj.Filename}  to  ${obj.TargetBase}`);
+            return util.simplePromise(obj);
+          })
+          .then(resolve)
+          .catch(function (err) {
+            console.error(`ERROR: ${err}`)
+            resolve();
+          });
+      });
+    });
+  };
+
+  run();
+})();
--- utils.js
+++ utils.js
@@ -1,106 +1,124 @@
-var gm = require('gm');
+(function () {
+    'use strict'
 
-var util = module.exports = {
+    var gm = require('gm');
 
-    stringToObject: function (str) {
-        str = str || '';
-        let obj = {};
+    var util = module.exports = {
 
-        let lines = str.match(/[^\r\n]+/g);
-        if (!!lines) {
+        stringToObject: function (str) {
+            str = str || '';
+            let obj = {};
 
-            for (let i = 0; i < lines.length; i++) {
-                let splitted = lines[i].split('=');
-                if (!!splitted[0] && !!splitted[1]) {
-                    let val = splitted[1].toLowerCase() == 'unknown' ? null : splitted[1];
-                    obj[splitted[0]] = val;
+            let lines = str.match(/[^\r\n]+/g);
+            if (!!lines) {
+
+                for (let i = 0; i < lines.length; i++) {
+                    let splitted = lines[i].split('=');
+                    if (!!splitted[0] && !!splitted[1]) {
+                        let val = splitted[1].toLowerCase() == 'unknown' ? null : splitted[1];
+                        obj[splitted[0]] = val;
+                    }
                 }
             }
-        }
 
-        return obj;
-    },
+            return obj;
+        },
 
-    // https://stackoverflow.com/a/7712335
-    stringToDateTime: function (str) {
-        str = str || '';
-        var pattern = /(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})/;
-        var dateArray = pattern.exec(str);
-        if (!dateArray || dateArray.length < 6) return null;
+        // https://stackoverflow.com/a/7712335
+        stringToDateTime: function (str) {
+            str = str || '';
+            var pattern = /(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})/;
+            var dateArray = pattern.exec(str);
+            if (!dateArray || dateArray.length < 6) return null;
 
-        var dateObject = new Date(
-            (+dateArray[1]),
-            (+dateArray[2]) - 1, // Careful, month starts at 0!
-            (+dateArray[3]),
-            (+dateArray[4]),
-            (+dateArray[5]),
-            (+dateArray[6])
-        );
+            var dateObject = new Date(
+                (+dateArray[1]),
+                (+dateArray[2]) - 1, // Careful, month starts at 0!
+                (+dateArray[3]),
+                (+dateArray[4]),
+                (+dateArray[5]),
+                (+dateArray[6])
+            );
 
-        return dateObject;
-    },
+            return dateObject;
+        },
 
-    getImageObject: function (file) {
-        let format = 'SourcePath=' + file + '\n' +
-            'Extension=%m\n' +
-            'ImageUniqueId=%[EXIF:ImageUniqueId]\n' +
-            'DateTime=%[EXIF:DateTime]';
+        getImageObject: function (file) {
+            let format = 'SourcePath=' + file + '\n' +
+                'Extension=%m\n' +
+                'ImageUniqueId=%[EXIF:ImageUniqueId]\n' +
+                'DateTime=%[EXIF:DateTime]';
 
-        return new Promise(function (resolve, reject) {
-            gm(file).identify(format, function (err, res) {
-                let exif = util.stringToObject(res);
+            return new Promise(function (resolve, reject) {
+                gm(file).identify(format, function (err, res) {
+                    let exif = util.stringToObject(res);
 
-                if (err || !exif) {
-                    console.info(`Failed to parse ${file}`);
-                    reject(err);
-                } else {
-                    resolve(exif);
-                }
+                    if (err || !exif) {
+                        console.info(`Failed to parse ${file}`);
+                        reject(err);
+                    } else {
+                        resolve(exif);
+                    }
+                });
             });
-        });
-    },
+        },
 
-    uuidv4: function (delimiter) {
-        if (typeof delimiter === 'undefined') delimiter = '-';
-        // return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        let format = 'xxxxxxxx' + delimiter + 'xxxx' + delimiter + '4xxx' + delimiter + 'yxxx' + delimiter + 'xxxxxxxxxxxx';
-        return format.replace(/[xy]/g, function (c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    },
+        uuidv4: function (delimiter) {
+            if (typeof delimiter === 'undefined') delimiter = '-';
+            // return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let format = 'xxxxxxxx' + delimiter + 'xxxx' + delimiter + '4xxx' + delimiter + 'yxxx' + delimiter + 'xxxxxxxxxxxx';
+            return format.replace(/[xy]/g, function (c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        },
 
-    simplePromise: function (obj) {
-        return new Promise(function (resolve, reject) {
-            resolve(obj);
-        });
-    },
+        simplePromise: function (obj) {
+            return new Promise(function (resolve, reject) {
+                resolve(obj);
+            });
+        },
 
-    targetPathFromImageObj: function (base, obj) {
-        let year = obj.DateTime.getFullYear().toString();
-        let month = (obj.DateTime.getMonth() + 1).toString();
-        let day = obj.DateTime.getDate().toString();
-        let hour = (obj.DateTime.getHours() + 1).toString();
-        let minute = obj.DateTime.getMinutes().toString();
-        let second = obj.DateTime.getSeconds().toString();
-        let ms = obj.DateTime.getMilliseconds().toString();
+        targetPathFromImageObj: function (base, obj) {
+            let year = obj.DateTime.getFullYear().toString();
+            let month = (obj.DateTime.getMonth() + 1).toString();
+            let day = obj.DateTime.getDate().toString();
+            let hour = (obj.DateTime.getHours() + 1).toString();
+            let minute = obj.DateTime.getMinutes().toString();
+            let second = obj.DateTime.getSeconds().toString();
+            let ms = obj.DateTime.getMilliseconds().toString();
 
-        if (month < 10) month = `0${month}`;
-        if (day < 10) day = `0${day}`;
-        if (hour < 10) hour = `0${hour}`;
-        if (minute < 10) minute = `0${minute}`;
-        if (second < 10) second = `0${second}`;
-        if (ms < 10) ms = `00${ms}`;
-        else if (ms < 100) ms = `0${ms}`;
+            if (month < 10) month = `0${month}`;
+            if (day < 10) day = `0${day}`;
+            if (hour < 10) hour = `0${hour}`;
+            if (minute < 10) minute = `0${minute}`;
+            if (second < 10) second = `0${second}`;
+            if (ms < 10) ms = `00${ms}`;
+            else if (ms < 100) ms = `0${ms}`;
 
-        let basePath = `${base}\\${year}\\${month}\\${day}`;
-        let dateStamp = `${year}${month}${day}`;
-        let timeStamp = `${hour}${minute}${second}${ms}Z`;
-        let fullStamp = `${dateStamp}T${timeStamp}.${obj.Extension.toLowerCase()}`;
-        let fileName = `IMG_${fullStamp}`;
-        return { basePath: basePath, fileName: fileName, fullPath: function () { return `${basePath}\\${fileName}`; } };
-    },
+            let basePath = `${base}\\${year}\\${month}\\${day}`;
+            let dateStamp = `${year}${month}${day}`;
+            let timeStamp = `${hour}${minute}${second}${ms}Z`;
+            let fullStamp = `${dateStamp}T${timeStamp}.${obj.Extension.toLowerCase()}`;
+            let fileName = `IMG_${fullStamp}`;
+            return { basePath: basePath, fileName: fileName, fullPath: function () { return `${basePath}\\${fileName}`; } };
+        },
 
+        processArray: function (array, fn) {
+            var index = 0;
 
+            return new Promise(function (resolve, reject) {
 
-};
+                function next() {
+                    if (index < array.length) {
+                        fn(array[index++]).then(next, reject);
+                    } else {
+                        resolve();
+                    }
+                }
+                next();
+            });
+        },
+
+    };
+})();
